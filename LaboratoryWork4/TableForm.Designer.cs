97315128546077d95c2c59ad09c26ac04cfbdfb5
Tugarev <mikhail-tugarev@mail.ru>
2022-12-24 01:30:21 +0700

    Создана структура проекта SalaryPaymentGUI

--- LaboratoryWork4/TableForm.Designer.cs
+++ LaboratoryWork4/TableForm.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace LaboratoryWork4
 {
-    partial class Form1
+    partial class TableForm
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -29,10 +29,17 @@ namespace LaboratoryWork4
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
