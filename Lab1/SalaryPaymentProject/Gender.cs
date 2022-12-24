b49fccb730558c525e8806d1555c7b63b29f5363
Tugarev <mikhail-tugarev@mail.ru>
2022-12-24 11:22:07 +0700

    Сформирован проект SalaryPaymentProject

--- Lab1/SalaryPaymentProject/Gender.cs
+++ Lab1/SalaryPaymentProject/Gender.cs
@@ -1,1 +1,13 @@
-﻿
+﻿namespace SalaryPaymentProject
+{
+    /// <summary>
+    /// Enum of person gender
+    /// </summary>
+    public enum Gender
+    {
+        Male,
+        Female,
+        Other,
+        Unknown
+    }
+}
